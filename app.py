4564e2d8030655c46806a570531501374d82dc14
Mpilo Mnyandu <mpmnyswgc025@student.wethinkcode.co.za>
2026-03-19 15:52:54 +0200

    added lifestyle feature to form

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    transport = request.form['transport']
+    diet = request.form['diet']
+    electricity = request.form['electricity']
+
+    return f"""
+    <h2>Your Input:</h2>
+    <p>Transport: {transport}</p>
+    <p>Diet: {diet}</p>
+    <p>Electricity: {electricity} kWh</p>
+    """
+
+if __name__ == '__main__':
+    app.run(debug=True)
