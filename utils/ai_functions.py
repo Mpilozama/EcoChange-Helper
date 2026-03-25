ded56cafcbcfcadb5f32353d96e524ccc8fed1c5
Mpilo Mnyandu <mpmnyswgc025@gmail.com>
2026-03-25 21:12:04 +0200

    added apis to get real data for accurate climate data

--- utils/ai_functions.py
+++ utils/ai_functions.py
@@ -1,3 +1,7 @@
+import requests
+
+
+
 def calculate_footprint(data):
     transport_type = data.get('transport_mode', 'walking')
     distance = float(data.get('distance', 0))
@@ -10,7 +14,7 @@ def calculate_footprint(data):
 
     transport_co2 = distance * factors.get(transport_type, 0)
 
-    # Assume average diet & electricity for MVP
+  
     diet_co2 = 75
     electricity_co2 = 200
 
@@ -56,3 +60,33 @@ def get_personalized_tips(transport):
             "Encourage others to adopt sustainable transport methods.",
             "Support local infrastructure that promotes safe cycling."
         ]
+    
+def get_real_climate_data(city):
+
+    geo_url = f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=1&language=en&format=json"
+    
+    try:
+        geo_res = requests.get(geo_url).json()
+        if not geo_res.get('results'):
+            return "Data Unavailable", "We couldn't locate this specific region's climate sensors."
+        
+        lat = geo_res['results'][0]['latitude']
+        lon = geo_res['results'][0]['longitude']
+
+        aq_url = f"https://air-quality-api.open-meteo.com/v1/air-quality?latitude={lat}&longitude={lon}&current=us_aqi,pm10,pm2_5"
+        aq_res = requests.get(aq_url).json()
+        
+        aqi = aq_res['current']['us_aqi']
+        
+
+        if aqi > 100:
+            risk = "Poor Air Quality detected."
+            insight = "This region shows high particulate matter, often linked to industrial inequality (SDG 10)."
+        else:
+            risk = "Stable Air Quality."
+            insight = "Current levels are sustainable, but urban growth by 2030 requires proactive greening."
+            
+        return risk, insight
+
+    except Exception as e:
+        return "Connection Error", "The AI is currently unable to reach local climate sensors."
