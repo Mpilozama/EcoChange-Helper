bdf8f0be24e83cf170c08a0d9b2d9613ca42d816
Mpilo Mnyandu <mpmnyswgc025@gmail.com>
2026-03-26 13:16:58 +0200

    added air quality data to get_climae_data function

--- utils/ai_functions.py
+++ utils/ai_functions.py
@@ -7,12 +7,30 @@ def get_climate_data(city):
     search_url = f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=1"
     geo_response = requests.get(search_url).json() #Data
 # will add error handling later
+
+#CO-ordinates:
     lat = geo_response["results"][0]["latitude"]
     lon = geo_response["results"][0]["longitude"]
     city_name = geo_response["results"][0]["name"]
 
+    # Air Qualuty data
+    aq_url = f"https://air-quality-api.open-meteo.com/v1/air-quality?latitude={lat}&longitude={lon}&current=pm2_5,ozone"
+    aq_response = requests.get(aq_url).json()
 
-    pass
+    # Dig into the JSON boxes to get the numbers
+    pm25 = aq_response["current"]["pm2_5"]
+    ozone = aq_response["current"]["ozone"]
+
+
+    return {
+        "city": city_name,
+        "lat": lat,
+        "lon": lon,
+        "pm25": pm25,
+        "ozone": ozone
+    }
+
+
 
 
 def get_climate_advice(city):
